09b73a55a1cd18c43ad9a0df16c7f90716b342d3
Icy-Thought <icy-thought@pm.me>
2022-08-27 23:21:18 +0200

    [λ] (nvim): more deperecated packages -> archived

---
+++ config/nvim/agasaya/modules/which-key.lua
@@ -0,0 +1,14 @@
+require("which-key").setup({
+    key_labels = {
+        ["<space>"] = "SPC",
+        ["<cr>"] = "RET",
+        ["<tab>"] = "TAB",
+    },
+    window = {
+        border = "rounded",
+        position = "bottom",
+        margin = { 1, 0, 1, 0 },
+        padding = { 1, 1, 1, 1 },
+        winblend = 0,
+    },
+})
