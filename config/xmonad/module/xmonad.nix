9bfd166283d38e5d8551149c14127ad9d98e414c
Icy-Thought <icy-thought@pm.me>
2022-12-07 08:23:24 +0100

    re-order for clarity

---
+++ config/xmonad/module/xmonad.nix
@@ -0,0 +1,65 @@
+{ inputs
+, options
+, config
+, lib
+, pkgs
+, ...
+}:
+with lib;
+with lib.my; {
+  options.modules.desktop.xmonad = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf config.modules.desktop.xmonad.enable {
+    modules.desktop = {
+      envProto = "x11";
+      toolset.fileBrowse = {
+        nautilus.enable = true;
+      };
+      extensions = {
+        fcitx5.enable = true;
+        mimeApps.enable = true; # mimeApps -> default launch application
+        picom.enable = true;
+        dunst.enable = true;
+        rofi.enable = true;
+        taffybar.enable = true;
+      };
+    };
+
+    environment.systemPackages = with pkgs; [
+      libnotify
+      playerctl
+      gxmessage
+      xdotool
+      xclip
+      feh
+    ];
+
+    services.xserver = {
+      enable = true;
+      displayManager.defaultSession = "none+xmonad";
+      windowManager.xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+        enableConfiguredRecompile = true;
+        # haskellPackages = with pkgs; [];
+        # ghcArgs = [];
+        # xmonadCliArgs = [];
+        config = "${config.snowflake.configDir}/xmonad/xmonad.hs";
+      };
+    };
+
+    hm.services = {
+      network-manager-applet.enable = true;
+      status-notifier-watcher.enable = true;
+    };
+
+    hm.xsession = {
+      enable = true;
+      numlock.enable = true;
+      preferStatusNotifierItems = true;
+      importedVariables = [ "GDK_PIXBUF_MODULE_FILE" ]; # Taffybar
+    };
+  };
+}
